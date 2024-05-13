1eb7129971e4ea845a1cdad617abca252d84d46a
MarkMahrous <markmahrous012@gmail.com>
2024-05-13 22:29:06 +0300

    adding implicit animation nd finalizing meals_app

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -19,7 +19,6 @@ class MealDetailsScreen extends ConsumerWidget {
       appBar: AppBar(
         actions: [
           IconButton(
-            icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
             onPressed: () {
               final wasAdded = ref
                   .read(favoriteMealsProvider.notifier)
@@ -30,6 +29,19 @@ class MealDetailsScreen extends ConsumerWidget {
                     wasAdded ? 'Added to favorites' : 'Removed from favorites'),
               ));
             },
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: ((child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
+              }),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
         title: Text(meal.title),
@@ -37,11 +49,14 @@ class MealDetailsScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              fit: BoxFit.cover,
-              height: 250,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+                height: 250,
+                width: double.infinity,
+              ),
             ),
             const SizedBox(height: 20),
             Text(
